b77ab6e1cff4782f2759aa361dbba07441045d29
Henrique Siqueira <henriquesm06@gmail.com>
2025-11-25 19:59:37 -0300

    v 1.2-b

--- graph.cpp
+++ graph.cpp
@@ -24,24 +24,18 @@ class Graph {
 
     vector<string> stations;
     vector<Line> lines;
-    vector<vector<string>> station_connect_with;
+    vector<vector<int>> station_connect_with;
 
     int find_station (string station) {
         for (int i = 0; i < stations.size(); i++) if (stations[i] == station) return i;
         return -1;
     }
 
-    bool pull_station (string station) {
-        if (find_station(station) != -1) return false;
-        stations.push_back(station);
-        return true;
-    }
-
-    bool connect_stations (string station_1, string station_2) {
-        int i_1 = find_station(station_1), i_2 = find_station(station_2), i;
+    bool connect_stations (int station_1_id, int station_2_id) {
+        if (station_1_id < 0 || station_1_id >= stations.size() || station_2_id < 0 || station_2_id >= stations.size()) return false;
         bool found = false;
-        for (i = 0; i < station_connect_with[i_1].size(); i++) if (station_connect_with[i_1][i] == station_2) found = true;  
-        if (!found) station_connect_with[i_1].push_back(station_2);
+        for (int i = 0; i < station_connect_with[station_1_id].size(); i++) if (station_connect_with[station_1_id][i] == station_2_id) found = true;  
+        if (!found) station_connect_with[station_1_id].push_back(station_2_id);
         return !found;
     }
 
@@ -58,14 +52,14 @@ class Graph {
             tmp = i + 1;
             for (i = tmp; buffer[i] != '\n'; i++) {
                 if (buffer[i] == ',' && buffer[i + 1] == ' ') {
-                    pull_station(station_name);
+                    push_station(station_name);
                     lines.at(lines.size() - 1).stations.push_back(station_name);
                     station_name.clear();
                     i++;
                 }
                 else station_name += buffer[i];
             }
-            pull_station(station_name);
+            push_station(station_name);
             lines.at(lines.size() - 1).stations.push_back(station_name);
             station_name.clear();
         }
@@ -85,10 +79,16 @@ class Graph {
         station_connect_with.resize(stations.size());
         for (int i = 0; i < lines.size(); i++) {
             for (int j = 1; j < lines[i].stations.size(); j++) {
-                connect_stations(lines[i].stations[j], lines[i].stations[j - 1]);
-                connect_stations(lines[i].stations[j - 1], lines[i].stations[j]);
+                connect_stations(find_station(lines[i].stations[j]), find_station(lines[i].stations[j - 1]));
+                connect_stations(find_station(lines[i].stations[j - 1]), find_station(lines[i].stations[j]));
             }
         }
+    }
+
+    bool push_station (string station) {
+        if (find_station(station) != -1) return false;
+        stations.push_back(station);
+        return true;
     }
     
 };
